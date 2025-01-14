7db67c5f40d2e2f632c9d64563cb96ace8d7e821
Karel Korinek <karelkorinek@outlook.cz>
2025-01-14 20:43:50 +0100

    added PC components visualisation to table
    (cherry picked from commit c0b4b825fcaf463bca41956030bba1160550081c)

--- client/bazar-pc/src/PCComponent/PCComponentList.js
+++ client/bazar-pc/src/PCComponent/PCComponentList.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const PCComponentList = () => {
+
+    const [ PcComponents, setPcComponents] = useState({});
+
+    useEffect( () => {
+        getData("http://localhost:8080/bazar/component")
+        .then(data => setPcComponents(data));
+    },[]);
 
     return (
         <div >
             <table className="table table-bordered">
                 <thead>
-
-                </thead>
-                <tbody>
                     <tr>
                         <th>Název</th>
                         <th>Cena</th>
                         <th>Prodejce</th>
                     </tr>
+                </thead>
+                <tbody>
+                    { PcComponents.map((item) => {
+                        <tr>
+                            <td>{item.name}</td>
+                            <td>{item.price}</td>
+                        </tr>
+                    })}
                 </tbody>
             </table>
         </div>
