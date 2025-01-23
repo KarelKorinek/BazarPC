a7ae7d934188361fe52a026d90efb75b56738c2a
Karel Korinek <karelkorinek@outlook.cz>
2025-01-23 20:08:26 +0100

    added navigation bar with links
    (cherry picked from commit 74d1cef400d4d46a0b5dd27133480130967022a4)

--- client/bazar-pc/src/App.js
+++ client/bazar-pc/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 
 import {
@@ -10,17 +11,28 @@ import {
 
 import PCComponentList from "./PCComponent/PCComponentList";
 import PCComponentDetail from "./PCComponent/PCComponentDetail";
+import PCComponentForm from "./PCComponent/PCComponentForm";
 
 
 export function App() {
   return (
     <Router>
+      <div className="container-fluid">
+        <nav className="nav nav-pills justify-content-end">
+          <NavLink className="nav-link " to="/bazar" end>Hlavní stránka</NavLink>
+          <NavLink className="nav-link" to="bazar/component/create" end>Přidat inzerát</NavLink>
+          <NavLink className="nav-link" to="bazar/person/components" end>Mé inzeráty</NavLink>
+          <NavLink className="nav-link" to="bazar/person/create" end>Registrace</NavLink>
+          <NavLink className="nav-link" to="bazar/person/signin" end>Přihlásit se</NavLink>
+        </nav>
+      </div>
       <div>
         <Routes>
           <Route index element={<Navigate to={"/bazar"} />} />
           <Route path="/bazar">
             <Route index element={<PCComponentList />} />
             <Route path="detail/:id" element={<PCComponentDetail />} />
+            <Route path="component/create" element={<PCComponentForm />} />
           </Route>
         </Routes>
       </div>
