da7ec4cc46932f5ac572739059b1ef5f9f0fae5d
Karel Korinek <karelkorinek@outlook.cz>
2025-01-23 20:07:39 +0100

    created PC component form for an advert creation
    (cherry picked from commit 987bca0aafde24fe4ebf2e7ccb189b056b0faadd)

--- client/bazar-pc/src/utilities/fetch.js
+++ client/bazar-pc/src/utilities/fetch.js
@@ -9,3 +9,18 @@ export function getData(url) {
         })
         .catch(error => console.log(error));   
 }
+
+export function postData(url, data) {
+
+    const options = {
+        method: "POST",
+        body: data          
+    }
+
+    return fetch(url, options)
+        .then(response => {
+            if(response.ok) return response.json();
+            throw new Error("Error while getting data from server.");
+        })
+        .catch(error => console.log(error));   
+}
