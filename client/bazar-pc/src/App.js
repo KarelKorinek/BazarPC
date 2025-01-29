d6ca62e4834f690b5d3763b0c086c81fedbbea29
Karel Korinek <karelkorinek@outlook.cz>
2025-01-29 19:39:55 +0100

    added link to user form and user login in navigation bar

--- client/bazar-pc/src/App.js
+++ client/bazar-pc/src/App.js
@@ -12,7 +12,8 @@ import {
 import PCComponentList from "./PCComponent/PCComponentList";
 import PCComponentDetail from "./PCComponent/PCComponentDetail";
 import PCComponentForm from "./PCComponent/PCComponentForm";
-
+import UserForm from "./user/UserForm";
+import LoginForm from "./user/LoginForm";
 
 export function App() {
   return (
@@ -21,9 +22,9 @@ export function App() {
         <nav className="nav nav-pills justify-content-end">
           <NavLink className="nav-link " to="/bazar" end>Hlavní stránka</NavLink>
           <NavLink className="nav-link" to="bazar/component/create" end>Přidat inzerát</NavLink>
-          <NavLink className="nav-link" to="bazar/person/components" end>Mé inzeráty</NavLink>
-          <NavLink className="nav-link" to="bazar/person/create" end>Registrace</NavLink>
-          <NavLink className="nav-link" to="bazar/person/signin" end>Přihlásit se</NavLink>
+          <NavLink className="nav-link" to="bazar/user/components" end>Mé inzeráty</NavLink>
+          <NavLink className="nav-link" to="bazar/user/" end>Registrace</NavLink>
+          <NavLink className="nav-link" to="bazar/user/account" end>Přihlásit se</NavLink>
         </nav>
       </div>
       <div>
@@ -33,6 +34,8 @@ export function App() {
             <Route index element={<PCComponentList />} />
             <Route path="detail/:id" element={<PCComponentDetail />} />
             <Route path="component/create" element={<PCComponentForm />} />
+            <Route path="user" element={<UserForm />} />
+            <Route path="user/account" element={<LoginForm />} />
           </Route>
         </Routes>
       </div>
