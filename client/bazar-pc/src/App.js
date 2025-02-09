44f8146cc2aa1fe0e2fcf4606fe8347b5683093e
Karel Korinek <karelkorinek@outlook.cz>
2025-02-09 12:52:56 +0100

    implemented logout function and implemented user session data in navigation menu

--- client/bazar-pc/src/App.js
+++ client/bazar-pc/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-
 import {
   BrowserRouter as Router,
   Route,
@@ -14,17 +13,62 @@ import PCComponentDetail from "./PCComponent/PCComponentDetail";
 import PCComponentForm from "./PCComponent/PCComponentForm";
 import UserForm from "./user/UserForm";
 import LoginForm from "./user/LoginForm";
+import { useSession } from "./context/session";
+import { deleteData } from "./utilities/fetch";
 
 export function App() {
+
+  const { session, setSession } = useSession();
+
+  const handleLogout = () => {
+    deleteData("http://localhost:8080/bazar/user/account")
+      .finally( () => setSession({
+        data: null,
+        status: "unauthorized"
+      }));
+  }
+
   return (
     <Router>
       <div className="container-fluid">
         <nav className="nav nav-pills justify-content-end">
-          <NavLink className="nav-link " to="/bazar" end>Hlavní stránka</NavLink>
-          <NavLink className="nav-link" to="bazar/component/create" end>Přidat inzerát</NavLink>
-          <NavLink className="nav-link" to="bazar/user/components" end>Mé inzeráty</NavLink>
-          <NavLink className="nav-link" to="bazar/user/" end>Registrace</NavLink>
-          <NavLink className="nav-link" to="bazar/user/account" end>Přihlásit se</NavLink>
+          <li className="nav-item">
+            <NavLink className="nav-link " to="/bazar" end>Hlavní stránka</NavLink>
+          </li>
+
+          <>
+            { session.data ?
+                            <>
+                              <li className="nav-item">
+                                <NavLink className="nav-link" to="bazar/component/create" end>Přidat inzerát</NavLink>
+                              </li>
+                              <li className="nav-item">
+                                <NavLink className="nav-link" to="bazar/user/components" end>Mé inzeráty</NavLink>
+                              </li>
+                              <li className="nav-item">
+                                <button className="nav-link " onClick={handleLogout}>Odhlásit se</button>
+                              </li>
+                              <li className="nav-item nav-link text-primary fw-bold">
+                                {session.data.email}
+                              </li>
+                            </>
+                            : session.status === "loading" ?
+                            <>
+                              <div className="spinner-border spinner-border-sm" role="status"></div>
+                              <span className="visually-hiden">Loading...</span>
+                            </>
+                            :
+                            <>
+                              <li className="nav-item">
+                                <NavLink className="nav-link" to="bazar/user/" end>Registrace</NavLink>
+                              </li>
+                              <li className="nav-item">
+                                <NavLink className="nav-link" to="bazar/user/account" end>Přihlásit se</NavLink>
+                              </li>
+                            </>
+                                  
+            }
+          </>
         </nav>
       </div>
       <div>
